5e3ffcc2a5ecf908bfb85d9c74118f49a1d1f989
Aron Rodrigues <aronr@amazon.com>
2018-07-24 18:21:02 +0100

    Dealing with POST and template testing

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Project based on book Test-Driven Development with Python by Harry J. W. Percival
-```
+
+```bash
 # python3 manage.py test
 # python3 manage.py runserver
 ```
--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
-
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def _check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -32,19 +37,22 @@ class NewVisitorTest(unittest.TestCase):
         # When he hits <Enter> the page updates and now the page lists:
         # "1. Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(0.5)
 
-        table = self.browser.find_element_by_id("list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(any(row.text == "1. Buy peacock feathers" for row in rows),
-            "New to-do item does not appear in table")
-
+        self._check_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item
-        self.fail("Finish test"
-        )
+        
         # He enters "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id("new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(0.5)
 
         # The page updates again, now show both items on her list
+        self._check_for_row_in_list_table("1: Buy peacock feathers")
+        self._check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.fail("Finish test")
 
         # Edith wonders whether the site will remember her list. The shi sees that the site generated an
         # unique URL for her -- there is some explanatory text to that effect
