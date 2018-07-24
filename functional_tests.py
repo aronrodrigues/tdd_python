7a2857b40dbaf03c2dec30c1829e8689d066e421
Aron Rodrigues <aronr@amazon.com>
2018-07-24 13:11:18 +0100

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
-    
+
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
@@ -17,17 +19,28 @@ class NewVisitorTest(unittest.TestCase):
 
         # He notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Finish the test")
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-Do", header_text)
 
         # He is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id("new_item")
+        selg.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
         # He types "Buy peacock feathers" into a text box
+        inputbox.send_keys("Buy peacock feathers")
 
         # When he hits <Enter> the page updates and now the page lists:
         # "1. Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id("list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(any(row.text == "1. Buy peacock feathers" for row in rows))
+
 
         # There is still a text box inviting her to add another item
-
+        self.fail("Finish test"
+        )
         # He enters "Use peacock feathers to make a fly"
 
         # The page updates again, now show both items on her list
@@ -38,6 +51,7 @@ class NewVisitorTest(unittest.TestCase):
         # He visits the URL - her to-do list is still there
 
         # Satiesfied, He goes back to sleep
+
 
 if __name__ == "__main__":
     unittest.main(warnings="ignore")
