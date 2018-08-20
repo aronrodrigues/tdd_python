e487bc7cdda8743f5f44a7346a9e4ceb7a5f03ab
Aron Rodrigues <aronr@amazon.com>
2018-08-20 11:23:41 +0100

    Different lists per user

--- lists/views.py
+++ lists/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
+    return render(request, "home.html")
 
-    new_item_text = request.POST.get("item_text")
-    if new_item_text:
-        Item.objects.create(text=new_item_text)
-        #item = Item()
-        #item.text = item_text
-        # item.save()
-        return redirect('/')
 
-    items = Item.objects.all()
-    return render(request, "home.html", {
-        "items": items
-    })
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
+
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
+
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
